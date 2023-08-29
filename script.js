57048b07865f9d3045de451ab9efeb0ecd5c9911
Anshu Prateek <anshu.prateek@grab.com>
2023-08-29 21:13:50 +0800

    working copy

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+
+  var main = new Splide( '#main-carousel', {
+  type       : 'fade',
+  heightRatio: 0.4,
+  fixedWidth : 480,
+  fixedHeight: 200,
+  pagination : false,
+  arrows     : false,
+  cover      : true,
+} );
+
+var thumbnails = new Splide( '#thumbnail-carousel', {
+  rewind          : true,
+  fixedWidth      : 104,
+  fixedHeight     : 58,
+  isNavigation    : true,
+  gap             : 10,
+  focus           : 'center',
+  pagination      : false,
+  cover           : true,
+  dragMinThreshold: {
+    mouse: 4,
+    touch: 10,
+  },
+  breakpoints : {
+    640: {
+      fixedWidth  : 66,
+      fixedHeight : 38,
+    },
+  },
+} );
+
+main.sync( thumbnails );
+main.mount();
+thumbnails.mount();
+  
+} );
+
 // Function to dynamically insert <li> elements into a given <ul> element
 function insertLiElements(count, htmlContent, ulElement) {
   for (let i = 1; i < count; i++) {
@@ -70,4 +109,53 @@ const fontBgHeightSlider = document.getElementById('font-bg-height-slider');
 fontBgHeightSlider.addEventListener('input', function() {
   const fontBgHeightValue = fontBgHeightSlider.value + 'px';
   targetParagraph.style.height = fontBgHeightValue;
+});
+
+
+document.getElementById("mex-form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the form from submitting traditionally
+
+  var formData = new FormData(this); // Create a FormData object from the form
+
+  var xhr = new XMLHttpRequest(); // Create a new XMLHttpRequest object
+
+  xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          var result = JSON.parse(xhr.responseText); // Assign the response from the server to a variable
+          const bylinesContainer = document.getElementById('bylines');
+          for (const language in result) {
+            if (result.hasOwnProperty(language)) {
+              const bylines = result[language];
+              console.log(`Language: ${language}`);
+              const listItem = document.createElement('li');
+              listItem.textContent = language;
+              bylinesContainer.appendChild(listItem);
+
+              // Loop through each phrase in the language
+              for (const byline of bylines) {
+                const listItem = document.createElement('li');
+                listItem.textContent = byline;
+                listItem.addEventListener('click', function() {
+                  overlayText.innerText = byline
+                });
+                bylinesContainer.appendChild(listItem);
+              }
+            }
+          }
+      }
+  };
+
+  xhr.open("POST", "http://m3api.e3l.in:5000/copywrite", true); // Replace with your server endpoint
+  xhr.send(formData); // Send the form data
+
+});
+
+
+const deleteButton = document.getElementById('deleteTaglines');
+const myList = document.getElementById('bylines');
+
+deleteButton.addEventListener('click', function() {
+  while (myList.firstChild) {
+    myList.removeChild(myList.firstChild);
+  }
 });
